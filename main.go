9056f90ef63f76a43a81989366bcbbc164539d0b
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-20 23:51:59 +0300

    Add page with response status 404

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
 	} else if r.URL.Path == "/contact" {
 		w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("<h1>404: The page you are looking doesn't exists :(</h1>"))
 	}
 }
 
