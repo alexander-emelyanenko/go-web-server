cd3e56303ef1c37ae25f13196128334dee8d4e40
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 02:59:17 +0300

    Add views and views handler

--- main.go
+++ main.go
@@ -4,37 +4,35 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/alexander-emelyanenko/go-web-server/views"
+
 	"github.com/julienschmidt/httprouter"
 )
 
-type notFoundHandler struct{}
-
-func (n *notFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("<h1>404: The page you're looking for doesn't exists</h1>"))
-}
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
+	if err := homeView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
-}
-
-func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Frequently asked questions</h1>"))
+	if err := contactView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
 	router := httprouter.New()
 	router.GET("/", home)
 	router.GET("/contact", contact)
-	router.GET("/faq", faq)
-	router.NotFound = &notFoundHandler{}
+
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
