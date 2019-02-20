ae08cbf39ccf8138b1175aa1dabb7c137750da2f
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 00:00:11 +0300

    Handle routes with gorilla/mux

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
-	} else if r.URL.Path == "/contact" {
-		w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("<h1>404: The page you are looking doesn't exists :(</h1>"))
-	}
+	w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
 }
 
 func main() {
-	http.HandleFunc("/", handleFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
