89323c5f0d6cc28c637bf301ea654785bca94120
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 00:06:14 +0300

    Add FAQ page

--- main.go
+++ main.go
@@ -16,9 +16,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Frequently asked questions</h1>"))
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
