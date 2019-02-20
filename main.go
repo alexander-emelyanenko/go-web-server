0e6c33f2457eb78ee0b2f5a3447f923e5173a0f3
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 00:51:57 +0300

    Implemented julienschmidt/httprouter instead of gorilla/mux

--- main.go
+++ main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
-}
+type notFoundHandler struct{}
 
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte("<h1>Frequently asked questions</h1>"))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
+func (n *notFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte("<h1>404: The page you're looking for doesn't exists</h1>"))
 }
 
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Welcome to main page of our web site!</h1>"))
+}
+
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Soon here will be our contacts... I hope</h1>"))
+}
+
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Frequently asked questions</h1>"))
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = http.HandlerFunc(notFound)
-	http.ListenAndServe(":3000", r)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.NotFound = &notFoundHandler{}
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
