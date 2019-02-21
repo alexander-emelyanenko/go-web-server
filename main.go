43a0a148748d05379971fa4e50474cd64bff2c51
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 03:32:26 +0300

    Bootstraping our views and working with rendering

--- main.go
+++ main.go
@@ -12,23 +12,25 @@ import (
 var homeView *views.View
 var contactView *views.View
 
-func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
+func must(err error) {
+	if err != nil {
 		panic(err)
 	}
+}
+
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w, nil); err != nil {
-		panic(err)
-	}
+	must(contactView.Render(w, nil))
 }
 
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	router := httprouter.New()
 	router.GET("/", home)
--- views/view.go
+++ views/view.go
@@ -2,14 +2,26 @@ package views
 
 import (
 	"html/template"
+	"net/http"
+	"path/filepath"
 )
 
-type View struct {
-	Template *template.Template
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+
+	return files
 }
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -17,5 +29,15 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
+}
+
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
