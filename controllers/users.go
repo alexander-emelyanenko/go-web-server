3873f156c7c625fd8f251df48d3daec7bd65b0cc
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-21 23:32:43 +0300

    Move sign up logic to users controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,26 @@
 package controllers
 
-type Users struct{}
+import (
+	"net/http"
+
+	"github.com/alexander-emelyanenko/go-web-server/views"
+)
+
+// Users struct describes our controller
+type Users struct {
+	NewView *views.View
+}
+
+// NewUsers method returns Users struct
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+// New method handles request
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/alexander-emelyanenko/go-web-server/controllers"
 	"github.com/alexander-emelyanenko/go-web-server/views"
-
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
 var homeView *views.View
 var contactView *views.View
-var signupView *views.View
 
 func must(err error) {
 	if err != nil {
@@ -19,30 +18,26 @@ func must(err error) {
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(homeView.Render(w, nil))
 }
 
-func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
-	router := httprouter.New()
-	router.GET("/", home)
-	router.GET("/contact", contact)
-	router.GET("/signup", signup)
+	usersController := controllers.NewUsers()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/signup", usersController.New)
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
