a638526525d1fde0b95be012101468c75d537234
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-24 14:41:50 +0300

    Refactoring automigration

--- models/users.go
+++ models/users.go
@@ -65,9 +65,18 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	return &user, nil
 }
 
-func (us *UserService) DestructiveReset() {
-	us.db.DropTableIfExists(&User{})
-	us.db.AutoMigrate(&User{})
+func (us *UserService) AutoMigrate() error {
+	if err := us.db.AutoMigrate(&User{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (us *UserService) DestructiveReset() error {
+	if err := us.db.DropTableIfExists(&User{}).Error; err != nil {
+		return err
+	}
+	return us.AutoMigrate()
 }
 
 func NewUserService(connectionInfo string) (*UserService, error) {
