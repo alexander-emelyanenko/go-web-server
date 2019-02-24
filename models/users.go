5624528bfeaaef15fedff4bc30f5d9efb992fbd8
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-25 01:28:30 +0300

    Adding Remember and RememberHash fields to User model

--- models/users.go
+++ models/users.go
@@ -20,7 +20,9 @@ type User struct {
 	Name         string
 	Email        string `gorm:"not null;unique_index"`
 	Password     string `gorm:"-"`
-	PasswordHash string `gorm:"no null"`
+	PasswordHash string `gorm:"not null"`
+	Remember     string `gorm:"-"`
+	RememberHash string `gorm:"not null;unique_index"`
 }
 
 type UserService struct {
