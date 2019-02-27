7612af6cce0603b2eaed2b6e122687fc04f2ee33
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-27 21:26:32 +0300

    Creating the GalleryService

--- models/galleries.go
+++ models/galleries.go
@@ -9,3 +9,19 @@ type Gallery struct {
 	UserID uint   `gorm:"not_null; index"`
 	Title  string `gorm:"not_null"`
 }
+
+type GalleryService struct {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(galllery *Gallery) error {
+
+}
