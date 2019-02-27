9720b87a0cf56dabea7a3566aad627ac7310e548
alexander-emelyanenko <van-persie-11@yandex.ru>
2019-02-27 23:22:47 +0300

    Validating galleries

--- models/galleries.go
+++ models/galleries.go
@@ -6,6 +6,11 @@ import (
 
 var _ GalleryDB = &galleryGorm{}
 
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+	ErrTitleRequired  modelError = "models: title is required"
+)
+
 type Gallery struct {
 	gorm.Model
 	UserID uint   `gorm:"not_null; index"`
@@ -22,15 +27,54 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 	}
 }
 
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(
+		gallery,
+		gv.userIDRequired,
+		gv.titleRequired,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID < 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
 type GalleryService interface {
 	GalleryDB
 }
 
 type galleryService struct {
-	GalleryDB
-}
-
-type galleryValidator struct {
 	GalleryDB
 }
 
